5012a8f127511a83168afdac08d97518a776b3b3
shinya furumaki <furumakishinya@mac.com>
2019-02-02 17:23:08 +1300

    Convert to MVC pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by 古牧晋弥 on 2/02/19.
+//  Copyright © 2019 Shinya Furumaki. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
